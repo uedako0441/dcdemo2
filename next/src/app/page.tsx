c941d5276e8bda13dc54a73f6ecf1badb0d58973
uedako0441 <uedako0441@gmail.com>
2024-12-21 18:15:00 +0900

    color changed

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -1,20 +1,36 @@
 import Link from "next/link";
 import "./bubble.css"; // CSSファイル
+import "./index.css";
 
 export default function HomePage() {
   return (
     <main className="main">
-      
-      <h1 className="center-text">乗車支援アプリ「駅楽」何かお困りごとはございますか？</h1>
+      {/* メニューバー */}
+      <nav className="navbar">
+        <ul className="navbar-list">
+          <li className="navbar-item">
+            <Link href="/" className="navbar-link">ホーム</Link>
+          </li>
+          <li className="navbar-item">
+            <Link href="/about" className="navbar-link">アプリについて</Link>
+          </li>
+          <li className="navbar-item">
+            <Link href="/contact" className="navbar-link">お問い合わせ</Link>
+          </li>
+        </ul>
+      </nav>
 
-      <div className="button-container">
-        <Link href="/chat" className="bubble">チャット</Link>
-        <Link href="/map" className="bubble">駅構内地図</Link>
-        <Link href="/input" className="bubble">トイレの場所</Link>
-        <Link href="/slope" className="bubble">スロープ</Link>
-        <Link href="/minitokyo" className="bubble">MiniTokyo3D</Link>
-        <Link href="/input" className="bubble">予約</Link>
-        <Link href="/navi" className="bubble">乗換案内</Link>
+      {/* メインコンテンツ */}
+      <div className="content">
+        <h1 className="center-text">乗車支援アプリ「駅楽」</h1>
+
+        <div className="button-container">
+          <p className="position">
+            <Link href="/map" className="soap">駅構内地図</Link>
+          </p>
+          <Link href="/navi" className="soap">乗換案内</Link>
+          <Link href="/book" className="soap">予約履歴</Link>
+        </div>
       </div>
     </main>
   );
